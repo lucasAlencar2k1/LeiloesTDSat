713c25a71a90f5e9e9625625622d5a4bc43cb89c
lucasAlencar2k1 <129227211+lucasAlencar2k1@users.noreply.github.com>
2026-05-20 23:05:22 -0300

    Adicionando operação vender produto

--- src/ArquivoDeTeste.java
+++ src/ArquivoDeTeste.java
@@ -2,7 +2,9 @@
 public class ArquivoDeTeste {
     public static void main(String args[]) {
         
-        System.out.println("Teste!");
+        ProdutosDAO produtosDAO = new ProdutosDAO();
+        
+        produtosDAO.venderProduto(22);
         
     }
 }
